bdda019b87fe6465a32038e90d91a6e0591c1847
raoam488 <raoam488@gmail.com>
2022-01-17 16:19:07 -0800

    Day2 Completed

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:first_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+
+  }
+}
